c5915db0e6c1dd2986539c565555912c97a2e8b4
ilcadmin <ilcadmin@atlas04.ucsc.edu>
2017-06-26 00:00:07 -0700

    autocommit on Mon Jun 26 00:00:07 PDT 2017

--- src/processors_collective/wwyatt/processors/check_overlay.cc
+++ src/processors_collective/wwyatt/processors/check_overlay.cc
@@ -43,6 +43,14 @@ check_overlay check_overlay;
 static TFile* _rootfile;
 
 static TH1F* _energy;
+static TH1F* _energy1;
+static TH1F* _energy2;
+static TH1F* _energy3;
+static TH1F* _energy4;
+static TH1F* _energy5;
+static TH1F* _energy6;
+static TH1F* _energy7;
+static TH1F* _energy8;
 static double totalEnergy=0;
 
 check_overlay::check_overlay() : Processor("check_overlay") {
@@ -59,6 +67,14 @@ void check_overlay::init() {
     streamlog_out(DEBUG) << "   init called  " << std::endl ;
     _rootfile = new TFile("check_overlay.root","RECREATE");
     _energy = new TH1F("energy", "Energy Distribution", 300,0,6000);
+    _energy1 = new TH1F("energy1", "Energy Distribution 200-600", 300,200,600);
+    _energy2 = new TH1F("energy2", "Energy Distribution 650-1050", 300,650,1050);
+    _energy3 = new TH1F("energy3", "Energy Distribution 1050-1600", 300,1050,1600);
+    _energy4 = new TH1F("energy4", "Energy Distribution 1600-2100", 300,1600,2100);
+    _energy5 = new TH1F("energy5", "Energy Distribution 2100-2600", 300,2100,2600);
+    _energy6 = new TH1F("energy6", "Energy Distribution 2600-3200", 300,2600,3200);
+    _energy7 = new TH1F("energy7", "Energy Distribution 3200-3600", 300,3200,3600);
+    _energy8 = new TH1F("energy8", "Energy Distribution 3800-4200", 300,3800,4200);
     _nEvt = 0 ;
 }
 
@@ -84,6 +100,14 @@ void check_overlay::processEvent( LCEvent * evt ) {
 	  parser::printAllEvents(col);
 	}
 	_energy->Fill(tEnergy);
+	_energy1->Fill(tEnergy);
+	_energy2->Fill(tEnergy);
+	_energy3->Fill(tEnergy);
+	_energy4->Fill(tEnergy);
+	_energy5->Fill(tEnergy);
+	_energy6->Fill(tEnergy);
+	_energy7->Fill(tEnergy);
+	_energy8->Fill(tEnergy);
     }    
 }
 
