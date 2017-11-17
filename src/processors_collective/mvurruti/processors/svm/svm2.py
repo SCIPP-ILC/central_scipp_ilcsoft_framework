0d1bb7a8f0443bb31da78eaf17afd9094b908172
ilcadmin <ilcadmin@atlas04.ucsc.edu>
2017-11-17 00:00:10 -0800

    autocommit on Fri Nov 17 00:00:10 PST 2017

--- src/processors_collective/mvurruti/processors/svm/svm2.py
+++ src/processors_collective/mvurruti/processors/svm/svm2.py
@@ -13,19 +13,23 @@ roots = [
 
 c = ROOT.TCanvas()
 
-for file_name in roots:
-    print file_name
-    file = ROOT.TFile(file_name)
-    name = "V_Tru"
-    graph = file.Get(name)
+name = [V_Tru,V_Dtd,V_Dbl,S_Dtd,S_Dbl,S_Tru,M_Dtd,M_Dbl,M_Tru]
+
+for graphs in name
+
+    for file_name in roots:
+        print file_name
+        file = ROOT.TFile(file_name)
+        # name = "V_Tru"
+        graph = file.Get(graphs)
     
-    try:
-        graph.Draw("SAME")
-    except ReferenceError:
-        print("Tried to find a graph (%s) in the root file that was not there."%name)
-        print("This is what is in the root file:")
-        file.ls()
-        exit()
+        try:
+            graph.Draw("SAME")
+        except ReferenceError:
+            print("Tried to find a graph (%s) in the root file that was not there."%name)
+            print("This is what is in the root file:")
+            file.ls()
+            exit()
 
     
 time.sleep(4)
